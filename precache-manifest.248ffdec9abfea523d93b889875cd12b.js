6fa16fae77f63149ed5309efe3ea2fab8cb5c105
mohiuddin <mohiuddin0240@gmail.com>
2020-09-04 22:12:30 +0600

    Updates

---
+++ precache-manifest.248ffdec9abfea523d93b889875cd12b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94cca1374f7630a4d9b42d7ffa54e6b",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "55663968aa33055d2e74",
+    "url": "/amazon-clone/static/css/main.54dde6af.chunk.css"
+  },
+  {
+    "revision": "c3136534949caac6a8bf",
+    "url": "/amazon-clone/static/js/2.cf95e579.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.cf95e579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55663968aa33055d2e74",
+    "url": "/amazon-clone/static/js/main.7725a391.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
